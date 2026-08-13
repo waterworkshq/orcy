aa119a129411dbb86de0aa48a82fceed825376db
Vikas Agarwal <vikasagarwal101@users.noreply.github.com>
2026-08-13 12:10:53 +0530

    test(api): register habitat agent mail route in inventory snapshot

--- packages/api/src/test/routeInventory.test.ts
+++ packages/api/src/test/routeInventory.test.ts
@@ -33,6 +33,7 @@ import { attachmentRoutes } from "../routes/attachments.js";
 import { notificationPrefRoutes } from "../routes/notificationPreferences.js";
 import { chatIntegrationRoutes } from "../routes/chatIntegration.js";
 import { agentMessageRoutes } from "../routes/agentMessages.js";
+import { habitatAgentMailRoutes } from "../routes/habitatAgentMail.js";
 import { codeReviewWebhookRoutes } from "../routes/codeReviewWebhooks.js";
 import { ciCdWebhookRoutes } from "../routes/ciCdWebhooks.js";
 import { organizationRoutes } from "../routes/organizations.js";
@@ -60,6 +61,7 @@ async function registerApiRoutes(f: FastifyInstance) {
   await f.register(notificationPrefRoutes);
   await f.register(chatIntegrationRoutes);
   await f.register(agentMessageRoutes);
+  await f.register(habitatAgentMailRoutes);
   await f.register(codeReviewWebhookRoutes);
   await f.register(ciCdWebhookRoutes);
   await f.register(organizationRoutes);
